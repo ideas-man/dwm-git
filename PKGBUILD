f30aa2bee22fdc15d782dff78cbe63d24d53c493
ideas-man <ideasman.git@protonmail.com>
2025-03-10 23:21:07 +0100

    chore: bump to 6.5-2

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 _pkgname=dwm
 pkgname=$_pkgname-git
 pkgver=6.5
-pkgrel=1
+pkgrel=2
 pkgdesc="A dynamic window manager for X"
 url="http://dwm.suckless.org"
 arch=('i686' 'x86_64')
--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "xos4 terminus:size=16" };
+static const char dmenufont[]       = "xos4 terminus:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -14,8 +14,8 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray4 },
+	[SchemeSel]  = { col_gray1, col_gray4, col_gray4 },
 };
 
 /* tagging */
